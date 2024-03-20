b7a1c00c1368497dc3f88811f884d9817ba905c8
Davronn <davronmahamadjonov8@gmai.com>
2024-03-20 16:55:10 +0500

    .

--- src/components/ProductDetelis.jsx
+++ src/components/ProductDetelis.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "../sass/detelis.scss";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import arrow from "../assets/arrow.svg";
 
 function ProductDetelis() {
   const { postId } = useParams();
@@ -10,7 +11,7 @@ function ProductDetelis() {
 
   const fetchProduct = async (postId) => {
     try {
-      const res = await axios.get(`http://localhost:3000/cards/${postId}`);
+      const res = await axios.get(`http://localhost:3000/shop/${postId}`);
       const data = await res.data;
       console.log(data);
       setProduct(data);
@@ -18,7 +19,6 @@ function ProductDetelis() {
       setError(error);
       console.log(error.message);
     } finally {
-        
     }
   };
 
@@ -28,14 +28,40 @@ function ProductDetelis() {
 
   return (
     <>
-      <div className="container pr_d">
-        <Link to={-1}>
-          <button>Back</button>
-        </Link>
-        <div className="detelis">
-          <img src={product.image} alt="" />
-          <h1>{product.title}</h1>
+      <div className="detelis">
+        <div className="info">
+          <div className="container">
+            <div className="info_text ">
+              <Link to="/">
+                <p className="opacity">Home</p>
+              </Link>
+              <img src={arrow} alt="" />
+              <Link to="/shop">
+                <p className="opacity">Shop</p>
+              </Link>
+              <img src={arrow} alt="" />
+              <div className="line"></div>
+              <p>{product.title}</p>
+            </div>
+          </div>
         </div>
+        <div className="container">
+          <div className="product_detal">
+            <div className="product_detal-imgs">
+              <div className="imgg-line">
+                <img src={product.image} alt={product.title} />
+                <img src={product.image} alt={product.title} />
+                <img src={product.image} alt={product.title} />
+                <img src={product.image} alt={product.title} />
+              </div>
+              <img className="img" src={product.image} alt="" />
+            </div>
+            <div className="product_detal-text"></div>
+          </div>
+        </div>
+      </div>
+      <div className="scrolto">
+        <h1>Scroll👆🏻</h1>
       </div>
     </>
   );
